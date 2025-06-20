37d172587c159f8a6ee809cfb2224dd6ce32bca1
MANIK <136956963+ManikSheoran@users.noreply.github.com>
2025-06-20 18:39:28 +0530

    setup DB

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:yourpassword@localhost/smartinventory"
+from sqlalchemy.orm import Session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+DATABASE_URL = "postgresql://postgres:mashis01@localhost:5432/smartinventory"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .database import Base, engine
 from .routes import sales  # import your routes
+from sqlalchemy.orm import Session
+from app.database import get_db
 
 app = FastAPI()
 
@@ -9,3 +11,6 @@ Base.metadata.create_all(bind=engine)
 
 # Include your routers
 app.include_router(sales.router, prefix="/api/sales", tags=["Sales"])
+@app.get("/test-db")
+def test_db_connection(db: Session = Depends(get_db)):
+    return {"msg": "Connected to PostgreSQL"}
