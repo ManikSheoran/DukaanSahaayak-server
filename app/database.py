c906a7df64f142f4f673e60be1db0e713366c904
MANIK <136956963+ManikSheoran@users.noreply.github.com>
2025-07-16 19:52:52 +0530

    Add dockerfile

--- app/database.py
+++ app/database.py
@@ -2,13 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-DATABASE_URL = "postgresql://postgres:mashis01@localhost:5432/smartinventory"
+DATABASE_URL = os.environ.get("DB_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
